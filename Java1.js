41180766c493e4468201eb268d4c4f544bfed381
Chris <45544080+thechrisfiles@users.noreply.github.com>
2019-02-12 20:23:15 +0100

    Update Java1.js

--- Java1.js
+++ Java1.js
@@ -1,4 +1,4 @@
-
+/* Función que hace funcionar el reloj */
 setInterval(function movida() {
     let fecha = new Date();
     let reloj = fecha.getHours() + " : " + fecha.getMinutes() + " : " + fecha.getSeconds(); 
